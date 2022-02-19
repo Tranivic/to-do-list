668c3b5958636887e454b704084762fd38a549cd
Auvic <victor.trani1@gmail.com>
2022-02-19 15:47:48 -0300

    Delete expecific task added

--- script.js
+++ script.js
@@ -2,39 +2,55 @@
 var clearAll = document.getElementById("clear-all");
 var btnAdd = document.getElementById("add-item");
 var list = document.getElementById("list");
-var tasks = [];
+var tasks = []
+counter = 0;
 
-//Counter to identifier the tasks
-var counter = 0;
-
-
-//Event Listeners
 btnAdd.addEventListener("click", addTask);
+list.addEventListener("click", getButtonId)
 
-
-//Function to print the task on the list and save into a array
 function addTask() {
-    if (counter < 5) {
-        counter++;
-        var taskContent = document.getElementById("taskName").value;
-        var task = {
-            Id: counter,
-            Content: taskContent,
-        };
+    counter++
+    var taskContent = document.getElementById("taskName").value
+    var object = { Content: taskContent, Id: counter, }
+    tasks.push(object)
 
-        tasks.push(task);
+    attScreen()
+}
 
-        list.innerHTML +=
-            `
+function attScreen() {
+    list.innerHTML = "";
+
+    for (var i = 0; i < tasks.length; i++) {
+        list.innerHTML += `
         <div class="item">
-                <label for="">
-                    ${taskContent}
+                <label>
+                    ${tasks[i].Content}
                 </label>
                 <div class="btn">
-                    <button class="btn-done"><i class="fa-solid fa-check"></i></button><button class="btn-remove"><i class="fa fa-x" aria-hidden="true"></i></button>
+                    <button class="btn-done"><i class="fa-solid fa-check"></i></button><button class="btn-remove remove"><i id="${tasks[i].Id}" class=" btn-remove fa fa-x"></i></button>
                 </div>
             </div>
-         `
+        `
+    }
+}
+
+
+function getButtonId(e) {
+
+    if (e.target.classList.contains('btn-remove')) {
+        let btnId = e.target.id
+
+        removeTask(btnId)
     }
 
+}
+
+function removeTask(btnId) {
+
+    for (let i = 0; i <= counter; i++) {
+        if (tasks[i].Id == btnId) {
+            tasks.splice(i, 1);
+            attScreen()
+        }
+    }
 }
