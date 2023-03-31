00d541964518f0d2577a0be4ecad95e16356e74f
Victor <victor.trani1@gmail.com>
2023-03-31 19:41:38 -0300

    chore: core more legibile for uniasselvi apresentation

--- script.js
+++ script.js
@@ -1,158 +1,143 @@
-//Variables
-var input = document.getElementById("taskName")
-const btnAdd = document.getElementById("add-item");
-const list = document.getElementById("list");
-var checkbox = document.getElementById("chk");
-var tasks = []
+// Selecionando elementos do DOM
+const buttonToAdd = document.getElementById('button-to-add-task');
+const listOfTasks = document.getElementById('list-of-tasks');
+let taskInput = document.getElementById('task-input');
+let themeCheckBox = document.getElementById('chk');
 
+// Inicializando array vazio de tarefas
+let tasks = [];
 
-//Event Listeners
-btnAdd.addEventListener("click", addTask);
-list.addEventListener("click", getButtonId);
-list.addEventListener("click", markAsDone);
-checkbox.addEventListener("click", changeTheme)
-input.addEventListener("keyup", e => {
-    if (e.keyCode === 13) {
-        e.preventDefault();
-        addTask()
-    }
-})
+// Event Listeners
+buttonToAdd.addEventListener('click', addTask);
+listOfTasks.addEventListener('click', getButtonId);
+themeCheckBox.addEventListener('click', changeTheme);
 
+// Adicionando evento de tecla pressionada no taskInput
+taskInput.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    addTask();
+  }
+});
 
-
-//This function will set the date on screen when page is loaded (Dayjs (library))
+// Função para carregar a data do dia no carregamento da página (utilizando a biblioteca Day.js para melhor formatação)
 window.onload = () => {
-    changeTheme()
-    document.getElementById("month").innerHTML = dayjs().format('MMM')
-    document.getElementById("day").innerHTML = dayjs().format('D')
-    document.getElementById("year").innerHTML = dayjs().format('YYYY')
-    document.getElementById("weekDay").innerHTML = dayjs().format('dddd')
-    setAsVisitor()
-}
+  document.getElementById('month').innerHTML = dayjs().format('MMM');
+  document.getElementById('day').innerHTML = dayjs().format('D');
+  document.getElementById('year').innerHTML = dayjs().format('YYYY');
+  document.getElementById('weekDay').innerHTML = dayjs().format('dddd');
+  setAsVisitor();
+};
 
+// Função para alternar o tema entre escuro e claro.
 function changeTheme() {
-    var body = document.querySelector('body');
-
-    if (checkbox.checked) {
-
-        body.style.backgroundColor = "#FCFBF7"
-        body.style.color = "#4E4D5B"
-
-    } else {
-
-        body.style.backgroundColor = "#403F4D"
-        body.style.color = "#FAFAF8"
-    }
-
+  const pageBody = document.querySelector('body');
+  if (themeCheckBox.checked) {
+    pageBody.style.backgroundColor = '#FCFBF7';
+    pageBody.style.color = '#4E4D5B';
+  } else {
+    pageBody.style.backgroundColor = '#403F4D';
+    pageBody.style.color = '#FAFAF8';
+  }
 }
 
+// Função para adicionar a nova tarefa no array de tarefas
 function addTask() {
-    let taskContent = document.getElementById("taskName").value
-    if (taskContent == "") {
-        confirm("Empty task")
-    } else {
-        localStorage.counter++
-        let object = { Content: taskContent, Id: localStorage.counter, Done: false }
-        SaveDataToLocalStorage(object)
-        tasks.push(object)
-        document.getElementById("taskName").value = ""
-        attScreen()
-    }
+  let taskContent = taskInput.value.trim('');
+
+  if (taskContent == '') {
+    confirm('Preencha o campo da nova tarefa');
+    return;
+  }
+
+  const newTaskId = localStorage.counter++;
+
+  const newTaskObject = {
+    content: taskContent,
+    id: newTaskId,
+    done: false,
+  };
+  saveNewTaskToLocalStorage(newTaskObject);
+  tasks.push(newTaskObject);
+  attScreen();
 }
 
+// Função para pegar o id do botão clicado, para identificar a task especifica.
 function getButtonId(e) {
-
-    if (e.target.classList.contains('btn-remove')) {
-        let btnId = e.target.id
-
-        removeTask(btnId)
-
-    } else if (e.target.classList.contains('btn-done')) {
-
-        let btnId = e.target.id
-
-        markAsDone(btnId)
-
-    }
-
+  if (e.target.classList.contains('btn-remove')) {
+    let btnId = e.target.id;
+    removeTask(btnId);
+  } else if (e.target.classList.contains('btn-done')) {
+    let btnId = e.target.id;
+    markAsDone(btnId);
+  }
 }
 
+// Função que remove a task do array de tasks
 function removeTask(btnId) {
-    for (let i = 0; i <= tasks.length; i++) {
-        if (tasks[i].Id == btnId) {
-            tasks.splice(i, 1);
-            localStorage.setItem('localTasks', JSON.stringify(tasks))
-            attScreen()
-        }
-    }
+  const clickedTaskIndex = tasks.findIndex((element) => element.id == btnId);
+  tasks.splice(clickedTaskIndex, 1);
+  localStorage.setItem('localTasks', JSON.stringify(tasks));
+  attScreen();
 }
 
+// Função que adiciona a task no array de tasks
 function markAsDone(btnId) {
-
-    for (let i = 0; i <= tasks.length; i++) {
-        if (tasks[i].Id == btnId) {
-            if (tasks[i].Done == false) {
-                tasks[i].Done = true
-                localStorage.setItem('localTasks', JSON.stringify(tasks))
-                attScreen()
-            } else {
-                tasks[i].Done = false
-                localStorage.setItem('localTasks', JSON.stringify(tasks))
-                attScreen()
-            }
-        }
-    }
+  const clickedTask = tasks.find((element) => element.id == btnId);
+  const taskIsDone = clickedTask.done;
+  clickedTask.done = !taskIsDone;
+  localStorage.setItem('localTasks', JSON.stringify(tasks));
+  attScreen();
 }
 
+// Função para atualizar os dados na tela sempre que necessário
 function attScreen() {
-    tasks = JSON.parse(localStorage.getItem('localTasks'))
-    list.innerHTML = "";
-    document.getElementById("taskName").value = "";
+  tasks = JSON.parse(localStorage.getItem('localTasks'));
+  listOfTasks.innerHTML = '';
+  taskInput.value = '';
 
-    for (var i = 0; i <= tasks.length; i++) {
-
-        if (tasks[i].Done == false) {
-            list.innerHTML += `
+  tasks.forEach((element) => {
+    if (element.done === false) {
+      listOfTasks.innerHTML += `
             <div class="item">
             <label>
-            ${tasks[i].Content}
+            ${element.content}
             </label>
             <div class="btn">
-            <button class="btn-done"><i id="${tasks[i].Id}" class=" btn-done fa-solid fa-check"></i></button><button class="remove"><i id="${tasks[i].Id}" class=" btn-remove fa fa-x"></i></button>
+            <button class="btn-done"><i id="${element.id}" class=" btn-done fa-solid fa-check"></i></button><button class="remove"><i id="${element.id}" class=" btn-remove fa fa-x"></i></button>
                 </div>
                 </div>
-        `
-        } else {
-
-            list.innerHTML += `
+        `;
+    } else {
+      listOfTasks.innerHTML += `
         <div class="item item-done">
         <label>
-        ${tasks[i].Content}
+        ${element.content}
         </label>
         <div class="btn">
-        <button class="btn-done"><i id="${tasks[i].Id}" class=" btn-done fa-solid fa-check"></i></button><button class="remove"><i id="${tasks[i].Id}" class=" btn-remove fa fa-x"></i></button>
+        <button class="btn-done"><i id="${element.id}" class=" btn-done fa-solid fa-check"></i></button><button class="remove"><i id="${element.id}" class=" btn-remove fa fa-x"></i></button>
         </div>
             </div>
-        `
-
-        }
+        `;
     }
+  });
 }
 
+// Função que seta a primeira vez em que o usuario acessa o site.
 function setAsVisitor() {
-    if (typeof Storage !== "undefined") {
-        if (!localStorage.counter) {
-            localStorage.counter = 0
-            localStorage.localTasks = null
-        } else{
-            attScreen()
-        }
+  if (typeof Storage !== 'undefined') {
+    if (!localStorage.counter || localStorage.counter != Number) {
+      localStorage.counter = 0;
+      localStorage.localTasks = null;
+    } else {
+      attScreen();
     }
+  }
 }
 
-function SaveDataToLocalStorage(data) {
-    var a = [];
-    a = JSON.parse(localStorage.getItem('localTasks')) || [];
-    a.push(data);
-    localStorage.setItem('localTasks', JSON.stringify(a));
+// Esta função salva o objeto da nova task no array do local storage
+function saveNewTaskToLocalStorage(data) {
+  let localSavedTasks = JSON.parse(localStorage.getItem('localTasks')) || [];
+  localSavedTasks.push(data);
+  localStorage.setItem('localTasks', JSON.stringify(localSavedTasks));
 }
